049d3c2ac756c361171b7d55276ce3c347b700cb
Rashika <rashikarawat01@gmail.com>
2021-04-26 10:20:02 +0530

    changes in models

--- models/User.js
+++ models/User.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
 
     username: {
         type: String,
-        required:[true, "Please provide a username"]
+        required:[true, "Please provide a username"],   
+        unique:true
 
     },
     profession: {
